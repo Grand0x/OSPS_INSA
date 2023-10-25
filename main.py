85dbf57fad59802137a8763e8db5c8677bfcf5d7
Grand0x <46326627+Grand0x@users.noreply.github.com>
2023-10-25 10:19:01 +0000

    wip

--- main.py
+++ main.py
@@ -1,62 +1,48 @@
 import os
 import sys
 import mmap
-import signal
 import time
+import signal
+import select
 
 # Chemins des tubes nommés
 DWTUBE_PATH = "dwtube1"
 WDTUBE_PATH = "wdtube1"
 PING_PONG_LIMIT = 5
+WATCHDOG_PING_TIMEOUT = 5
 
 # Temps (en secondes) que le watchdog attend avant de considérer qu'il n'y a pas de communication
 NO_COMMUNICATION_LIMIT = 3
 
-# Temps (en secondes) que le watchdog attend une réponse après avoir envoyé un SIGUSR1
-PING_RESPONSE_TIMEOUT = 5
-
-# Variable pour stocker l'état du ping (répondu ou non)
-ping_response_received = False
-
 
 def create_named_pipes():
-  """
-    Crée les tubes nommés si nécessaire.
-    """
+  """Crée les tubes nommés si nécessaire."""
   if not os.path.exists(DWTUBE_PATH):
     os.mkfifo(DWTUBE_PATH)
   if not os.path.exists(WDTUBE_PATH):
     os.mkfifo(WDTUBE_PATH)
 
 
-def signal_handler(signum, frame):
+def send_watchdog_ping_and_wait():
   """
-    Gestionnaire pour les signaux reçus.
-    """
-  global ping_response_received
-  if signum == signal.SIGUSR2:
-    ping_response_received = True
-
-
-def response_to_ping(signum, frame, watchdog_pid):
-  """Envoyer un signal SIGUSR2 en réponse à un SIGUSR1."""
-  os.kill(watchdog_pid, signal.SIGUSR2)
-
-
-def send_ping_and_wait(pid):
+  Envoie un "watchdog_ping" via le tube et attend un "watchdog_pong" en retour.
   """
-    Envoie un signal SIGUSR1 (ping) et attend un SIGUSR2 (pong).
-    """
-  global ping_response_received
-  ping_response_received = False
-  os.kill(pid, signal.SIGUSR1)
+  # Envoyer un watchdog_ping
+  with open(DWTUBE_PATH, 'w') as dwtube:
+    dwtube.write("watchdog_ping")
+    dwtube.flush()
+
+  # Attendre un watchdog_pong avec une lecture non bloquante
   start_time = time.time()
-
-  while not ping_response_received and (time.time() -
-                                        start_time) < PING_RESPONSE_TIMEOUT:
-    time.sleep(0.1)
-
-  return ping_response_received
+  with open(WDTUBE_PATH, 'r') as wdtube:
+    while (time.time() - start_time) < WATCHDOG_PING_TIMEOUT:
+      ready, _, _ = select.select([wdtube], [], [], 0.1)
+      if 0 < len(ready):
+        message = wdtube.read()
+        if message == "watchdog_pong":
+          print("return TRUE")
+          return True
+  return False
 
 
 def watchdog_process():
@@ -66,24 +52,26 @@ def watchdog_process():
 
   pid = os.fork()
   if pid == 0:
-    main_with_pipes(os.getpid())  # Passer le PID du watchdog
+    main_with_pipes()
     sys.exit(0)
   elif pid < 0:
     print("fork() failed in watchdog")
     sys.exit(-1)
   else:
-    signal.signal(signal.SIGUSR2, signal_handler)
-    time.sleep(2)  #pour laisser le temps au serveur de se lancer
+    time.sleep(2)  # pour laisser le temps au serveur de se lancer
     no_communication_count = 0
     while True:
-      if not send_ping_and_wait(pid):
+      if not send_watchdog_ping_and_wait():
         no_communication_count += 1
+        print(
+            f"[WATCHDOG] Watchdog n'a pas reçu de communication pendant {NO_COMMUNICATION_LIMIT} secondes."
+        )
         if no_communication_count >= NO_COMMUNICATION_LIMIT:
           print("[WATCHDOG] Pas de réponse. Redémarrage du serveur principal.")
           os.kill(pid, signal.SIGTERM)
           pid = os.fork()
           if pid == 0:
-            main_with_pipes(os.getpid())
+            main_with_pipes()
             sys.exit(0)
           elif pid < 0:
             print("fork() failed in watchdog retry")
@@ -94,31 +82,30 @@ def watchdog_process():
       time.sleep(1)
 
 
-def main_with_pipes(watchdog_pid):
+def main_with_pipes():
   """Gère la communication entre le serveur principal et secondaire."""
   print("[MAIN] Serveur principal démarré.")
-
-  # Configurer la réponse aux pings du watchdog
-  signal.signal(signal.SIGUSR1,
-                lambda s, f: response_to_ping(s, f, watchdog_pid))
-
   with mmap.mmap(-1, 10) as shared_memory:
     pid = os.fork()
     if pid == 0:
       print("[SECONDARY] Serveur secondaire démarré.")
       for i in range(PING_PONG_LIMIT):
-        if i == 3:
-          print("fake server block (10s)")
-          time.sleep(10)
         with open(DWTUBE_PATH, 'r') as dwtube:
-          dwtube.read()
-        if shared_memory[:4].decode('utf-8') == "ping":
-          print("[SECONDARY] Ping reçu. Envoi de pong.")
-          shared_memory.seek(0)
-          shared_memory.write(b"pong")
-          with open(WDTUBE_PATH, 'w') as wdtube:
-            wdtube.write("pong_sent")
-            wdtube.flush()
+          message = dwtube.read()
+          if message == "ping_sent":
+            if i == 3:
+              print("fake server block (5s)")
+              time.sleep(5)
+            print("[SECONDARY] Ping reçu. Envoi de pong.")
+            shared_memory.seek(0)
+            shared_memory.write(b"pong")
+            with open(WDTUBE_PATH, 'w') as wdtube:
+              wdtube.write("pong_sent")
+              wdtube.flush()
+          elif message == "watchdog_ping":
+            with open(WDTUBE_PATH, 'w') as wdtube:
+              wdtube.write("watchdog_pong")
+              wdtube.flush()
       print("[SECONDARY] Serveur secondaire terminé.")
       sys.exit(0)
     elif pid < 0:
@@ -133,8 +120,8 @@ def main_with_pipes(watchdog_pid):
           dwtube.write("ping_sent")
           dwtube.flush()
         with open(WDTUBE_PATH, 'r') as wdtube:
-          wdtube.read()
-        if shared_memory[:4].decode('utf-8') == "pong":
+          message = wdtube.read()
+        if message == "pong_sent":
           print("[MAIN] Pong reçu.")
       os.wait()
       print("[MAIN] Serveur principal terminé.")
